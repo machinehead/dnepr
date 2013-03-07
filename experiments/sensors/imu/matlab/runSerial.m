94bb29d6d29792b8e893f97304b99de10480d868
ilia <nekhayiv@gmail.com>
2013-03-07 11:56:38 +0400

    Improved timecounting and added accelerometer moving average.

--- experiments/sensors/imu/matlab/runSerial.m
+++ experiments/sensors/imu/matlab/runSerial.m
@@ -16,8 +16,10 @@ plot(times, vels);
 f4 = figure();
 plot(times, coords);
 
-iter = 0;
+currTime = 0;
 tm1 = clock();
+
+accMovAvgSamples = 10;
 
 while exist('runkey', 'file')
   l = fgetl(port);
@@ -28,16 +30,20 @@ while exist('runkey', 'file')
   elseif strncmp(l, '!ANG,', 5)
       data = textscan(l, '!ANG,%f,%f,%f,GYRO,%d,%d,%d,ACC,%d,%d,%d,MAG,%f,%f,%f,%f\r\n',1);
       if size(data{1,13},1)
-        iter = iter + 1;
+        currTime = currTime + data{1,13};
         anglesLocal = num2cell([data{1,1:3}]);
         [roll, pitch, yaw] = deal(anglesLocal{:});
         dcm = [cosd(pitch)*cosd(yaw)  sind(roll)*sind(pitch)*cosd(yaw)-cosd(roll)*sind(yaw)  cosd(roll)*sind(pitch)*cosd(yaw)+sind(roll)*sind(yaw);
                cosd(pitch)*sind(yaw)  sind(roll)*sind(pitch)*sind(yaw)+cosd(roll)*cosd(yaw)  cosd(roll)*sind(pitch)*sind(yaw)-sind(roll)*cosd(yaw);
                -sind(pitch)           sind(roll)*cosd(pitch)                                 cosd(roll)*cosd(pitch)];
         angles = [angles; cell2mat(anglesLocal)];
-        accsLocal = (double([data{1,7:9}]) - offsetAccel) + (dcm'*[0. 0. 256.]')';
+        accsLocal = double([data{1,7:9}]) - offsetAccel + (dcm'*[0. 0. 256.]')';
+        if size(accs,1) >= accMovAvgSamples - 1
+            prevAccsSum = sum(accs((size(accs,1)-accMovAvgSamples+2):size(accs,1),:),1);
+            accsLocal = (accsLocal + prevAccsSum) ./ accMovAvgSamples;
+        end
         accs = [accs; accsLocal];
-        times = [times; iter];
+        times = [times; currTime];
         vels = integrate(accs .* (8 * 9.81 / 2048.), 0.02);
         coords = integrate(vels, 0.02);
       end
