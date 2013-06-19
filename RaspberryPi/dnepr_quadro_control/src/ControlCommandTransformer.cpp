3e01a6d0aa78eb38d3a3bbcba4650322490e5792
root <root@ubuntu.ubuntu-domain>
2013-06-19 10:23:34 +0400

    мелкие правки

--- RaspberryPi/dnepr_quadro_control/src/ControlCommandTransformer.cpp
+++ RaspberryPi/dnepr_quadro_control/src/ControlCommandTransformer.cpp
@@ -1,9 +1,9 @@
 //Узел, занимающийся преобразованием команд полёта в "сырые" значения ручек управления (как с джойстика)
 #include <ros/ros.h>
 #include "dnepr_quadro_control/RawArduinoControl.h"
-#include "FlightDirection.h"
+#include "dnepr_quadro_control/FlightDirection.h"
 
-ros::Publisher* resultPublisher = 0;
+ros::Publisher resultPublisher = 0;
 
 void flightDirectionRecievedCallback( const dnepr_quadro_control::FlightDirection& /*msg*/ )
 {
@@ -15,8 +15,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "main_control_loop");
   ros::NodeHandle rosNode;
 
-  ros::Publisher rosPublisher = rosNode.advertise<RawArduinoControl>("raw_arduino_control", 1000);
-  resultPublisher = &rosPublisher;
+  resultPublisher = rosNode.advertise<RawArduinoControl>("raw_arduino_control", 1000);
 
   ros::Subscriber flight_direction_sub = rosNode.subscribe( "flight_direction", 1000, flightDirectionRecievedCallback );
 
